44e8d0028018d7a51110871d3fe3e31c2d64726a
Vladimir-Romanov131 <92376566+Vladimir-Romanov131@users.noreply.github.com>
2023-04-19 18:44:51 +0300

    1.2

--- src/skript.js
+++ src/skript.js
@@ -42,3 +42,30 @@ document.addEventListener("DOMContentLoaded", function (event) {
   checker.checkFile();
 });
 //
+function changeClass() {
+  const imgMain = document.getElementById("img-main");
+  const DivMain = document.getElementById("MainBackground");
+  const DivPanel = document.getElementById("PanelStyle");
+  const DivLinck = document.querySelectorAll("#Panel-Link");
+  const allParagraphs = document.querySelectorAll("p");
+  const allTitle = document.querySelectorAll("h1");
+  imgMain.classList.remove("img_intro-shadow");
+  imgMain.classList.add("img_intro-shadowBlack");
+
+  DivMain.classList.remove("MainBackground");
+  DivMain.classList.add("MainBackground-black");
+
+  DivPanel.classList.remove("PanelStyle");
+  DivPanel.classList.add("PanelStyle-black");
+
+  allParagraphs.forEach(function (paragraph) {
+    paragraph.style.color = "white";
+  });
+  allTitle.forEach(function (allTitle) {
+    allTitle.style.color = "white";
+  });
+
+  DivLinck.forEach(function (DivLinck) {
+    DivLinck.classList.replace("link", "link-black");
+  });
+}
