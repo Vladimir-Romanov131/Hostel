9c1a4e6e4f25c0c39a6c28a59561d3cf2b9bbc4b
Vladimir-Romanov131 <92376566+Vladimir-Romanov131@users.noreply.github.com>
2023-04-23 13:18:54 +0300

    1.9

--- src/calendar.js
+++ src/calendar.js
@@ -3,26 +3,30 @@ $(document).ready(function () {
     defaultView: "month",
     events: [],
     timeFormat: "YYYY-MM-DDTHH",
+    editable: false,
+    viewRender: function (view, element) {
+      // получаем данные из файла reservation.json
+      fetch("../reservation/reservation.json")
+        .then((response) => response.json())
+        .then((data) => {
+          const checkinDate = moment(data.checkinDate);
+          const checkoutDate = moment(data.checkoutDate);
+          const checkinDateStr = checkinDate.format("YYYY-MM-DD");
+          const checkoutDateStr = checkoutDate.format("YYYY-MM-DD");
+          console.log("checkinDate:", checkinDateStr);
+          console.log("checkoutDate:", checkoutDateStr);
+
+          calendar.fullCalendar("removeEvents");
+          calendar.fullCalendar("renderEvent", {
+            title: "Забронировано (номер: " + data.roomType + ")",
+            start: checkinDateStr,
+            end: checkoutDateStr,
+            color: "red",
+          });
+
+          calendar.fullCalendar("rerenderEvents");
+        })
+        .catch((error) => console.error(error));
+    },
   });
-
-  // получаем данные из файла reservation.json
-  fetch("../reservation/reservation.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const checkinDate = data.checkinDate;
-      const checkoutDate = data.checkoutDate;
-      console.log("checkinDate:", checkinDate);
-      console.log("checkoutDate:", checkoutDate);
-
-      // отображаем забронированные даты в календаре
-      calendar.fullCalendar("renderEvent", {
-        title: "Забронировано",
-        start: checkinDate,
-        end: checkoutDate,
-        color: "red",
-      });
-
-      calendar.fullCalendar("render");
-    })
-    .catch((error) => console.error(error));
 });
--- src/index.js
+++ src/index.js
@@ -56,6 +56,7 @@ app.on("activate", () => {
     createWindow();
   }
 });
+app.commandLine.appendSwitch("ignore-certificate-errors", "true");
 // ...................................................................................................
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
